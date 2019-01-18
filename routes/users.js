b02aced448e30afe5003bfb724f3bccd46e1f083
b.sampera <b.sampera@gmail.com>
2019-01-18 17:36:18 +0100

    conf

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
-router.get('/register', (req, res, nect) => {
-  res.send('REGISTER');
+const User = require('../models/user');
+
+
+router.post('/register', (req, res, nect) => {
+  let newUser = new User({
+    name: req.body.name, 
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  User.addUser(newUser, (err, user) => {
+    if(err) {
+      res.json({success: false, msg: 'Failed to register user'});
+    }else{
+      res.json({success: true, msg: 'User registered'});
+    }
+  });
 });
 
 router.post('/authenticate', (req, res, nect) => {
@@ -11,10 +29,6 @@ router.post('/authenticate', (req, res, nect) => {
 
 router.get('/profile', (req, res, nect) => {
   res.send('PROFILE');
-});
-
-router.get('/validate', (req, res, nect) => {
-  res.send('VALIDATE');
 });
 
 module.exports = router;
